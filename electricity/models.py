a3e8a15d06df0a1bbf19efaa2199bdff303743cc
Esteban Arteaga <mattesteban@outlook.es>
2024-11-30 09:34:31 -0500

    Diferente index para usuario autenticado

--- electricity/models.py
+++ electricity/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usage(models.Model):
+    device_name = models.CharField(max_length=30, null=False)
+    usage = models.DecimalField(null=False, default=1, decimal_places=2, max_digits=6)
+
+    def __str__(self) -> str:
+        return f"{self.device_name} - {self.usage}"
--- electricity/views.py
+++ electricity/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return render(request, 'index_loggedin.html')  # Template for logged-in users
+    else:
+        return render(request, 'index.html')  # Template for guests
