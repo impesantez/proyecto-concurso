eece8a557b741f84afbb1fce22c8a9f19b1a9d70
impesantez <impesantez@puce.edu.ec>
2024-11-30 09:49:15 -0500

    redirect a la página principal

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     #path("login/", views.CustomLoginView.as_view(), name='login'),
     path("signup/", views.signup, name="signup"), 
     path("login/", views.CustomLoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='electricity:index'), name='logout'),
 ]
