b84032df7018cdd16ebd52f79081b7330f80d5aa
Esteban Arteaga <mattesteban@outlook.es>
2024-11-30 21:55:05 -0500

    User added first_login field

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     name = models.CharField(max_length=30, null=False)
+    first_login = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
