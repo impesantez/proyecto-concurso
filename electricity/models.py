7d878757e6edd48503bb8d44fc757a1a4ea94fd5
Esteban Arteaga <mattesteban@outlook.es>
2024-11-30 22:18:34 -0500

    Calculator models created

--- electricity/models.py
+++ electricity/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+
 class Usage(models.Model):
     device_name = models.CharField(max_length=30, null=False)
     usage = models.DecimalField(null=False, default=1, decimal_places=2, max_digits=6)
 
     def __str__(self) -> str:
         return f"{self.device_name} - {self.usage}"
+    
+class Carbon_footprint(models.Model):
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    carbon = models.DecimalField(null=False, default=1, decimal_places=2, max_digits=6)
+
+    def __str__(self) -> str:
+        return f"{self.carbon}"
